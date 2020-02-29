3bd717851c54800d0e982213f539dc2219a39950
Santosh <santoshchoudhury5@gmail.com>
2020-02-29 11:08:00 +0530

    #client

--- resources/views/addclient.blade.php
+++ resources/views/addclient.blade.php
@@ -28,61 +28,66 @@
 	{{csrf_field()}}
 <table class="table table-responsive table-hover table-bordered table-striped" >
         <tr>
-            <td colspan="4" class="text-center bg-navy">ADD A NEW DEBITOR</td>
+            <td colspan="4" class="text-center bg-navy">ADD A NEW CLIENT</td>
         </tr>
         <tr>
-         <td><strong>CLIENT/DEPARTMENT NAME</strong><span style="color: red"> *</span></td>
+         <td><strong>DEPARTMENT NAME</strong><span style="color: red"> *</span></td>
          <td><input type="text" name="clientname" class="form-control" placeholder="Enter Client Name" required=""></td>
-         <td><strong>TAN NUMBER</strong><span style="color: red"> *</span></td>
+         <td><strong>DISTICT</strong><span style="color: red"> *</span></td>
+         <td>
+           <select name="district" class="form-control select2">
+             <option>aaa</option>
+             <option>bbb</option>
+           </select>
+         </td>
+        </tr>
+        <tr>
+        	<td><strong>DIVISION</strong><span style="color: red"> *</span></td>
+           <td>
+            <input type="text" name="division" class="form-control" placeholder="Enter Division Name"></td>
+           </td>
+           <td><strong>CONTACT NO</strong><span style="color: red"> *</span></td>
+           <td><input type="number" name="contact1" class="form-control" placeholder="Enter Contact No"></td>
+        </tr>
+        <tr>
+          <td><strong>ALTERNATIVE CONTACT NO</strong></td>
+           <td><input type="number" name="contact2" class="form-control" placeholder="Enter Alternative Contact No"></td>
+        	<td><strong>OFICE CONTACT NO</strong></td>
+           <td><input type="number" name="officecontact" class="form-control" placeholder="Enter Office  Contact No"></td>          
+        </tr>
+          <tr>
+            <td><strong>EMAIL</strong></td>
+            <td><input type="email" name="email" class="form-control" placeholder="Enter Email Here"></td>
+            <td><strong>GSTN</strong></td>
+           <td><input type="text" name="gstn" class="form-control" placeholder="Enter Client GST No"></td>          
+        </tr>
+        <tr>
+          <td><strong>PAN NO</strong></td>
+            <td><input type="text" name="panno" class="form-control" placeholder="Enter Client PAN No"></td>
+          <td><strong>TAN NUMBER</strong><span style="color: red"> *</span></td>
          <td><input type="text" name="tanno" class="form-control" placeholder="Enter Tan Number" required=""></td>
         </tr>
         <tr>
-        	<td><strong>CONTACT NO</strong><span style="color: red"> *</span></td>
-           <td><input type="number" name="contact1" class="form-control" placeholder="Enter Contact No"></td>
-           <td><strong>ALTERNATIVE CONTACT NO</strong></td>
-           <td><input type="number" name="contact2" class="form-control" placeholder="Enter Alternative Contact No"></td>
-        </tr>
-        <tr>
-        	<td><strong>OFICE CONTACT NO</strong></td>
-           <td><input type="number" name="officecontact" class="form-control" placeholder="Enter Office  Contact No"></td>
-        
-           	<td><strong>EMAIL</strong></td>
-           	<td><input type="email" name="email" class="form-control" placeholder="Enter Email Here"></td>
-          
-        </tr>
-
-             <tr>
-            <td><strong>GSTN</strong></td>
-           <td><input type="text" name="gstn" class="form-control" placeholder="Enter Client GST No"></td>
-        
-            <td><strong>PAN NO</strong></td>
-            <td><input type="text" name="panno" class="form-control" placeholder="Enter Client PAN No"></td>
-          
-        </tr>
-        <tr>
-        	<td><strong>RESIDENT ADDRESS</strong></td>
-        	<td><textarea name="residentaddress" class="form-control" placeholder="Enter Resident address"></textarea></td>
-
-        	<td><strong>OFFICE ADDRESS</strong><span style="color: red"> *</span></td>
-        	<td><textarea name="officeaddress" class="form-control" placeholder="Enter Office address" required=""></textarea></td>
-        </tr>
-        <tr>
-        	<td><strong>CITY</strong><span style="color: red"> *</span></td>
-         <td><input type="text" name="city" class="form-control" placeholder="Enter city Here" required=""></td>
-         <td><strong>DISTICT</strong><span style="color: red"> *</span></td>
-         <td><input type="text" name="dist" class="form-control" placeholder="Enter Distict Here" required=""></td>
-        </tr>
-        <tr>
-        	<td><strong>STATE</strong><span style="color: red"> *</span></td>
+          <td><strong>TIN NUMBER</strong><span style="color: red"> *</span></td>
+         <td><input type="text" name="tinno" class="form-control" placeholder="Enter Tin Number" required=""></td>
+         <td><strong>STATE</strong><span style="color: red"> *</span></td>
          <td><input type="text" name="state" class="form-control" placeholder="Enter state Here" required=""></td>
+        </tr>
+        <tr>
+          <td><strong>CITY</strong><span style="color: red"> *</span></td>
+         <td><input type="text" name="city" class="form-control" placeholder="Enter city Here" required=""></td>
          <td><strong>COUNTRY</strong><span style="color: red"> *</span></td>
          <td><input type="text" name="country" class="form-control" placeholder="Enter Country Here" required=""></td>
         </tr>
         <tr>
+          <td><strong>RESIDENT ADDRESS</strong></td>
+          <td><textarea name="residentaddress" class="form-control" placeholder="Enter Resident address"></textarea></td>
+          <td><strong>OFFICE ADDRESS</strong><span style="color: red"> *</span></td>
+          <td><textarea name="officeaddress" class="form-control" placeholder="Enter Office address" required=""></textarea></td>
+        </tr>
+        <tr>
         	<td><strong>ADDITIONAL INFO</strong><span style="color: red"> *</span></td>
         	<td><textarea name="additionalinfo" class="form-control" placeholder="Enter Addional Info"></textarea></td>
-          <td><strong>TIN NUMBER</strong><span style="color: red"> *</span></td>
-         <td><input type="text" name="tinno" class="form-control" placeholder="Enter Tin Number" required=""></td>
         </tr>
                 <tr>
             <td></td>
