031be6832f405b7dae3b72bad49567769a64ec2b
Santosh <santoshchoudhury5@gmail.com>
2020-03-13 16:13:25 +0530

    #comments

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2941,7 +2941,7 @@ public function approvedebitvoucheradmin(Request $request,$id)
                         ->where('useraccounts.type','COMPANY')
                         ->leftJoin('banks','useraccounts.bankid','=','banks.id')
                         ->get();
-return $requisitionpayments;
+//return $requisitionpayments;
            $userbankaccount=useraccount::select('useraccounts.*','banks.bankname','users.name')
            ->leftJoin('banks','useraccounts.bankid','=','banks.id')
            ->leftJoin('users','useraccounts.user','=','users.id')
